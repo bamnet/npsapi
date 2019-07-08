9234ad97d94797e15ffadd1c6973d03335bef4ed
Brian M <bmichalski@gmail.com>
2019-07-08 01:19:46 -0400

    Add a simple get park too.

--- parks.go
+++ parks.go
@@ -3,8 +3,12 @@ package npsapi
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"strings"
 )
+
+// ErrParkNotFound is a type of error returned when no parks were found.
+var ErrParkNotFound = errors.New("park not found")
 
 // Park basics include location, contact, and operating hours for each national park.
 type Park struct {
@@ -25,6 +29,27 @@ func (c *Client) ListParks(ctx context.Context) ([]Park, error) {
 	params := map[string]string{
 		"limit": "99999", // Fetch more parks than we have.
 	}
+	return c.fetchParks(ctx, params)
+}
+
+// GetPark finds a specific park with a 4-character code.
+func (c *Client) GetPark(ctx context.Context, code string) (*Park, error) {
+	params := map[string]string{
+		"parkCode": code,
+		"limit":    "1",
+	}
+
+	park, err := c.fetchParks(ctx, params)
+	if err != nil {
+		return nil, err
+	}
+	if len(park) >= 1 {
+		return &park[0], nil
+	}
+	return nil, ErrParkNotFound
+}
+
+func (c *Client) fetchParks(ctx context.Context, params map[string]string) ([]Park, error) {
 	resp, err := c.fetch(ctx, "parks", params)
 	if err != nil {
 		return nil, err
